665f48d9ee4f3fc141aac4aaf6b847b99395ded0
seguvega <seguvega@hotmail.com>
2020-08-03 17:07:57 -0500

    Falto COrregir un required

--- models/mysqlKeys.js
+++ models/mysqlKeys.js
@@ -1,13 +1,14 @@
 module.exports = {
-    /*
+
     DB: {
         host: 'localhost',
         user: 'root',
-        password: 'kvmr2108',
-        database: 'almacenes_jr2',
+        password: 'Velascogsg',
+        database: 'almacenes_jr',
         multipleStatements: true
-    }*/
+    }
 
+    /*
     ///Clever Cloud
     DB: {
         host: 'bmjd90hnoovqvagyqmil-mysql.services.clever-cloud.com',
@@ -16,16 +17,16 @@ module.exports = {
         database: 'bmjd90hnoovqvagyqmil',
         multipleStatements: true
     }
-
+    */
     ///Google Cloud
     //Necesito los ips de sus compus
     /*
-        DB: {
-            host: process.env.GCLOUD_host,
-            user: process.env.GCLOUD_user,
-            password: process.env.GCLOUD_password,
-            database: 'almacenes_jr',
-            multipleStatements: true
-        }
+    DB: {
+        host: process.env.GCLOUD_host,
+        user: process.env.GCLOUD_user,
+        password: process.env.GCLOUD_password,
+        database: 'almacenes_jr',
+        multipleStatements: true
+    }
     */
 };
